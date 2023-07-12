16e0f1abc9ace5e6e4b8db0c062b7e102312b32f
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-07-12 20:09:28 +0530

    added instruction verify 1st a/c is signer & 2nd a/c is some uninitialized storage

--- README.md
+++ README.md
@@ -28,3 +28,5 @@ src/processor.rs - that the heart of our program, here we handle all of the logi
 > in the processor.rs, we parse an instruction, serialized with borsh into bytes given as instruction_data, into Instruction enum and match which concrete instruction has been passed. For now, any other instruction is not supported.
 
 > Plan what we need as a state of our staking pool. We need an authority who owns the staking pool, the amount of staked tokens, the number of users, and how many rewards users earn per staked token.
+
+> process_initialize_pool function - We need to check that the first account is a signer as our program expects it to be Staking pool authority; we need to check that the second account is some storage and is not yet initialized. Once those constraints are satisfied, we can initialize the pool storage account with good defaults.
--- src/processor.rs
+++ src/processor.rs
@@ -23,3 +23,37 @@ pub fn process(
         }
     }
 }
+
+fn process_initialize_pool(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    rewards_per_token: u64,
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let signer = next_account_info(accounts.iter())?;
+    if !signer.is_signer {
+        return Err(StakingError::InvalidSigner.into());
+    }
+
+    let storage = next_account_info(accounts_iter)?;
+    if storage.owner != program_id {
+        return Err(StakingError::InvalidOwner.into());
+    }
+
+    let mut storage_data = PoolStorageAccount::try_from_slice(&storage.data.borrow())?;
+    if storage_data.is_initialized() {
+        return Err(StakingError::AlreadyInitialized.into());
+    }
+
+    storage_data.pool_authority = *signer.key;
+    storage_data.total_staked = 0u64;
+    storage_data.user_count = 0u64;
+    storage_data.rewards_per_token = rewards_per_token;
+    storage_data.is_initialized = true;
+
+    storage_data.serialize(&mut &mut storage.data.borrow_mut()[..])?;
+
+    msg!("Staking pool is initialized {:?}", storage_data);
+
+    Ok(())
+}
